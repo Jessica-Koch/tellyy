1d4b3b89a55019b938ace65c568f05f20da39b90
Jessica Koch <jessicakoch136@icloud.com>
2018-10-14 21:17:46 -0700

    update server file

--- server/src/server.js
+++ server/src/server.js
@@ -1,21 +1,12 @@
-// const http = require('http');
-// const app = require('../build/src/app');
-
-// const port = parseInt(process.env.PORT, 10) || 5000;
-// app.set('port', port);
-
-// const server = http.createServer(app);
-// server.listen(port);
-
-// server.on('listening', () => {
-//   console.log(`Server is listening on port ${server.address().port}`);
-// });
-const app = require('./app');
 const http = require('http');
-const server = http.createServer(app);
+const app = require('./app');
 
-server.listen(5000);
+const port = parseInt(process.env.PORT, 10) || 5000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
 
 server.on('listening', () => {
-  console.log('server is listening for requests on port 5000');
+  console.log(`Server is listening on port ${server.address().port}`);
 });
