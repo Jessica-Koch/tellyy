5023799454cb27af8108a54ce908e77d9cad4920
Jessica Koch <jessicakoch136@icloud.com>
2018-10-14 20:00:24 -0700

    update port to work with heroku

--- server/spec/integration/static_spec.js
+++ server/spec/integration/static_spec.js
@@ -1,15 +1,23 @@
-const request = require("request");
-const server = require("../../src/server");
-const base = "http://localhost:3000/";
+const request = require('request');
+const server = require('../../src/server');
+const base = 'http://localhost:3000/';
 
-describe("routes : static", () => {
-  describe("GET /", () => {
-    it("should return status code 200", (done) => {
+describe('routes : static', () => {
+  describe('GET /', () => {
+    it('should return status code 200', done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         done();
       });
     });
+  });
 
+  describe('GET /get-shows-popular', () => {
+    it('should return status code 200', done => {
+      request.get(base + 'get-shows-popular', (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
   });
 });
