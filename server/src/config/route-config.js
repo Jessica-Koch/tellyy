bf5b8cba65efb8d482d3dbc8d46c7654c167b255
Jessica Koch <jessicakoch136@icloud.com>
2018-10-14 18:53:08 -0700

    correct route config to import express to serve static files for production

--- server/src/config/route-config.js
+++ server/src/config/route-config.js
@@ -1,11 +1,12 @@
 const logger = require('morgan');
 const path = require('path');
-const staticFiles = express.static(
-  path.join(__dirname, '../../../filmView/build')
-);
+const express = require('express');
 module.exports = {
   init(app) {
     const staticRoutes = require('../routes/static');
+    const staticFiles = express.static(
+      path.join(__dirname, '../../../filmView/build')
+    );
     app.use(
       logger(
         ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
